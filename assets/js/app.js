3b67a68994746e2de3b79ec011bfae1133398584
Nich Secord <nicholassecord@gmail.com>
2019-06-07 14:22:18 -0700

    finalized structure and added selectors to app.js

---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+const characters = [
+    {
+        name: 'character1',
+        hp: 100,
+        baseAtk: 8,
+        counterAtk: 12,
+    },
+    {
+        name: 'character2',
+        hp: 100,
+        baseAtk: 8,
+        counterAtk: 12,
+    },
+    {
+        name: 'character3',
+        hp: 100,
+        baseAtk: 8,
+        counterAtk: 12,
+    },
+    {
+        name: 'character4',
+        hp: 100,
+        baseAtk: 8,
+        counterAtk: 12,
+    },
+];
+
+const charSelect = document.getElementById('character-select');
+const enemies = document.getElementById('enemies');
+const playerChar = document.getElementById('player-char');
+const targetChar = document.getElementById('target-char');
+const attackBtn = document.getElementById('atk-btn');
